f09d35ee847b7d433addcc7dc965d5ca72cff683
albilaga <albilaga.pradana@tonestro.com>
2024-11-29 11:16:21 +0700

    Enable ProGuard
    Added ProGuard configuration for release config

--- Tonestro.Maui.FFmpegMediaMetadataRetrieverSlim.UsageChecker/MainActivity.cs
+++ Tonestro.Maui.FFmpegMediaMetadataRetrieverSlim.UsageChecker/MainActivity.cs
@@ -9,8 +9,23 @@ public class MainActivity : Activity
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
-        var duration = Com.Tonestro.Ffmpegmediametadataretrieverslim.FFmpegMediaMetadataRetrieverSlim.GetDurationInMs(
-            "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4");
-        Console.WriteLine($"This video duration is {duration} ms");
+        var textView = this.FindViewById<TextView>(Resource.Id.label);
+        if (textView is null)
+        {
+            throw new NullReferenceException(nameof(textView));
+        }
+
+        try
+        {
+            const string videoUrl =
+                "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4";
+            var duration = Com.Tonestro.Ffmpegmediametadataretrieverslim.FFmpegMediaMetadataRetrieverSlim
+                .GetDurationInMs(videoUrl);
+            textView.Text = $"Duration: {duration}";
+        }
+        catch (Exception e)
+        {
+            textView.Text = e.ToString();
+        }
     }
 }
